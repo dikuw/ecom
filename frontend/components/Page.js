31c609ba5c9b8c32eabcfb7716b47bbffac4679d
dikuw <michael@dikuw.com>
2022-03-27 13:07:38 -0400

    add Page component

---
+++ frontend/components/Page.js
@@ -0,0 +1,5 @@
+export default function Page() {
+  return <div>
+    <h2>page component</h2>
+  </div>
+}
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,5 +1,5 @@
 export default function HomePage() {
-  return <div>
+  return <Page>
     <p>home</p>
-  </div>
+  </Page>
 }
