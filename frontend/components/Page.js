13261d80060d2d4e61746f9f8f197180e3a0d0a9
dikuw <michael@dikuw.com>
2022-03-27 13:15:07 -0400

    add prop types

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,6 +1,12 @@
+import PropTypes from 'prop-types';
+
 export default function Page({ children }) {
   return <div>
     <h2>page component</h2>
     { children }
   </div>
 }
+
+Page.prototypes = {
+  children: PropTypes.arrayOf(PropTypes.node),
+};
