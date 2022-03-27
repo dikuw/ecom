6f89454bae4dd2597a9be4edf30aa05738950a7a
dikuw <michael@dikuw.com>
2022-03-27 14:35:13 -0400

    add Logo styled component

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,12 +1,29 @@
 import Link from 'next/link';
-
+import styled from 'styled-components';
 import Nav from './Nav';
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  background: red;
+  transform: skew(-7deg);
+  a {
+    color: white;
+    text-decoration: none;
+    text-transform: uppercase;
+    padding: 0.5rem 1rem;
+  }
+`;
 
 export default function Heater() {
   return (
     <header>
       <div className="bar">
-        <Link href="/">Logo goes header</Link>
+        <Logo>
+          <Link href="/">Logo</Link>
+        </Logo>
       </div>
       <div className="sub-bar">
         <p>Search</p>
