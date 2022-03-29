0148dcc04424146c5e44df0d738aeaa76fd5564b
dikuw <michael@dikuw.com>
2022-03-29 18:02:27 -0400

    add nProgress

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,12 @@
 import Page from '../components/Page';
+import Router from 'next/router'
+import nProgress from 'nprogress';
+
+Router.events.on('routeChangeStart', () => nProgress.start());
+Router.events.on('routeChangeComplete', () => nProgress.done());
+Router.events.on('routeChangeError', () => nProgress.done());
+
+import '../components/styles/nprogress.css';
 
 export default function app({ Component, pageProps }) {
   return (
