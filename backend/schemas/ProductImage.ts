ec03eaa75f219e87b1c1785ddb742a7185941138
dikuw <michael@dikuw.com>
2022-04-11 19:58:29 -0400

    initial commit

---
+++ backend/schemas/ProductImage.ts
@@ -0,0 +1,19 @@
+import { cloudinaryImage } from "@keystone-next/cloudinary";
+import { text } from "@keystone-next/fields";
+import { list } from "@keystone-next/keystone/schema";
+
+export const cloudinary = {
+  cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+  apiKey: process.env.CLOUDINARY_CLOUD_NAME,
+  apiSecret: process.env.CLOUDINARY_CLOUD_NAME,
+}
+
+export const ProductImage = list({
+  fields: {
+    image: cloudinaryImage({
+      cloudinary,
+      label: 'Source'
+    }),
+    altTest: text(),
+  },
+});
