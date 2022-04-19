0a2905c7dac04942f5aa754ead0204d255a0b082
dikuw <michael@dikuw.com>
2022-04-19 17:53:30 -0400

    add gql query

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -1,4 +1,25 @@
+import gql from 'graphql-tag';
+
+const ALL_PRODUCTS_QUERY = gql`
+  query ALL_PRODUCTS_QUERY {
+    allProducts {
+      id
+      name
+      price
+      description
+      photo {
+        id
+        image {
+          publicUrlTransformed
+        }
+      }
+    }
+  }
+`;
+
 export default function Products() {
+  const {data, error, loading} = useQuery(ALL_PRODUCTS_QUERY);
+  
   return (
     <div>
       <p>products</p>
