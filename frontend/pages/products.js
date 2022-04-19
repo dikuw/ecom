c425d385717545a192d77bace82f2af12d43f2a7
dikuw <michael@dikuw.com>
2022-04-19 17:47:58 -0400

    add Products component

--- frontend/pages/products.js
+++ frontend/pages/products.js
@@ -1,5 +1,9 @@
+import Products from "../components/Products";
+
 export default function ProductPage() {
-  return <div>
-    <p>products</p>
-  </div>
+  return (
+    <div>
+      <Products />
+    </div>
+  )
 }
