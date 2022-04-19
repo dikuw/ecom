55ba375e2cba1608b4102463746c637b69a0097d
dikuw <michael@dikuw.com>
2022-04-19 18:30:56 -0400

    add list of products

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -1,3 +1,4 @@
+import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
 
 const ALL_PRODUCTS_QUERY = gql`
@@ -18,11 +19,19 @@ const ALL_PRODUCTS_QUERY = gql`
 `;
 
 export default function Products() {
-  const {data, error, loading} = useQuery(ALL_PRODUCTS_QUERY);
+  const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
   
+  if (loading) return <p>Loading...</p>;
+
+  if (error) return <p>Error: {error.message}</p>;
+
   return (
     <div>
-      <p>products</p>
+      <div>
+        {data.allProducts.map(product => (
+          <p key={product.id}>{product.name}</p>
+        ))}
+      </div>
     </div>
   )
 }
