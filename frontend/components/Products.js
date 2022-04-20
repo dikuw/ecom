f960572fd446e1e31308735c21f63e7f9a9d0ce1
dikuw <michael@dikuw.com>
2022-04-20 17:47:20 -0400

    add ProductsList grid

--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -1,5 +1,6 @@
 import { useQuery } from '@apollo/client';
 import gql from 'graphql-tag';
+import styled from 'styled-components';
 
 const ALL_PRODUCTS_QUERY = gql`
   query ALL_PRODUCTS_QUERY {
@@ -18,6 +19,12 @@ const ALL_PRODUCTS_QUERY = gql`
   }
 `;
 
+const ProductsList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+`;
+
 export default function Products() {
   const { data, error, loading } = useQuery(ALL_PRODUCTS_QUERY);
   
@@ -27,11 +34,11 @@ export default function Products() {
 
   return (
     <div>
-      <div>
+      <ProductsList>
         {data.allProducts.map(product => (
           <p key={product.id}>{product.name}</p>
         ))}
-      </div>
+      </ProductsList>
     </div>
   )
 }
